7bc6d63e5022e95764a98fc92177347c6a18e011
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-19 21:58:02 -0500

    Create para_nro01.py

---
+++ para_nro01.py
@@ -0,0 +1,13 @@
+#hallar la suma de las areas de un hexagono cuando el apotema se comprende de 4 a 16
+import os
+base_h=float(os.sys.argv[1])
+apotema=4
+
+#iterar la apotema
+while (apotema<17):
+    print(apotema)
+    apotema+=1
+    area=(6*base_h*apotema)/2
+
+#fin_while
+print("la suma de las areas de un hexagono es:", area)

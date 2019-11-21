fa6e2857f736b10613b005d5b1af1a0a5b1663f9
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-20 21:32:09 -0500

    Create deco_nro05.py

---
+++ deco_nro05.py
@@ -0,0 +1,17 @@
+#decodificar una correo que contenga "5","8","2"y"6"
+import os
+correo=os.sys.argv[1]
+
+#iterar correo
+for cifras in correo:
+    if (cifras=="5"):
+        print("j")
+    if (cifras=="8"):
+        print("brenis")
+    if (cifras=="2"):
+        print("l")
+    if (cifras=="6"):
+        print("gmail")
+#fin_iterar
+
+print("correo decodificado")

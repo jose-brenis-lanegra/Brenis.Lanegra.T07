33717b49148babcf5f8bcc6d3534b54d7ffd1c96
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-20 21:59:57 -0500

    Create lote.err.bat

---
+++ lote.err.bat
@@ -0,0 +1,10 @@
+python while_nro01.py 154 365
+python while_nro02.py 100
+python while_nro03.py 953 346
+python while_nro04.py 10 292
+python while_nro05.py 236 456 70
+python deco_nro01.py sabdyewdub
+python deco_nro02.py -<<-<-<-ndyt
+python deco_nro03.py q42ecvs53
+python deco_nro04.py sfecw2654nk-+
+python deco_nro05.py +-*/.ktxsb

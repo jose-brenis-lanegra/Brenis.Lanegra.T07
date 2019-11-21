8647db0cfe255a8810de770b85f83aa37552c117
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-20 21:51:54 -0500

    Create lote.bat

---
+++ lote.bat
@@ -0,0 +1,20 @@
+python while_nro01.py 365 154
+python while_nro02.py 98
+python while_nro03.py 346 953
+python while_nro04.py 100 29
+python while_nro05.py 456 236 74
+python para_nro01.py 35
+python para_nro02.py 3.14
+python para_nro03.py 24 15
+python para_nro04.py 3.14
+python para_nro05.py 3.14 25
+python deco_nro01.py xgtxtgxtxtg
+python deco_nro02.py .*././*/./../*/
+python deco_nro03.py +f<f<+f+f+<f
+python deco_nro04.py jd4j4d44jd4
+python deco_nro05.py 58626556528
+python range_nro01.py 35
+python range_nro02.py 3.14
+python range_nro03.py 24 15
+python range_nro04.py 3.14
+python range_nro05.py 3.14 25 
